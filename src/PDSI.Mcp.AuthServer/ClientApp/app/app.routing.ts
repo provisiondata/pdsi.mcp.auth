3ed67b2941ee1464e0a4e3d7a3c8da08a579e4a6
Doug Wilson <d@dkw.io>
2018-05-02 13:53:40 -0700

    WIP - Branding

--- src/PDSI.Mcp.AuthServer/ClientApp/app/app.routing.ts
+++ src/PDSI.Mcp.AuthServer/ClientApp/app/app.routing.ts
@@ -4,8 +4,6 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
 import { AuthGuard } from './guards/index';
  
 const appRoutes: Routes = [
@@ -13,8 +11,6 @@ const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
-    { path: 'counter', component: CounterComponent, canActivate: [AuthGuard] },
-    { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthGuard] },
     
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
